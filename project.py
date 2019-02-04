f8aef775478b8905cf8e71979839d55db1011bb9
Gregers Petersen <gregers@septima.dk>
2019-02-04 12:50:43 +0100

    Fixes empty description nodes

--- project.py
+++ project.py
@@ -77,7 +77,7 @@ class Project:
 
     self._project['Issues'].append({"title": item.title.text[item.title.text.index("]") + 2:len(item.title.text)],
       'key': item.key.text,
-      "body": item.description.text,
+      "body": (item.description.text if item.description.text else "empty description"),
       'created_at': self._convert_to_iso(item.created.text),
       'closed_at': closed_at,
       'updated_at': self._convert_to_iso(item.updated.text),
